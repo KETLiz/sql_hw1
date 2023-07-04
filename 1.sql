549c9a2edfa26c8bf64cff2646a9b86c73ce7c61
Li <varlamova_e.t@mail.ru>
2023-07-04 11:49:46 +0300

    1hw

---
+++ 1.sql
@@ -0,0 +1,5 @@
+INSERT INTO `hw`.`mobil_phones` (`product_name`, `manufacture`, `product_count`, `price`) VALUES ('iPhone X', 'Apple', '3', '76000');
+INSERT INTO `hw`.`mobil_phones` (`product_name`, `manufacture`, `product_count`, `price`) VALUES ('iPhone 8', 'Apple', '2', '51000');
+INSERT INTO `hw`.`mobil_phones` (`product_name`, `manufacture`, `product_count`, `price`) VALUES ('Galaxy S9', 'Samsung', '2', '56000');
+INSERT INTO `hw`.`mobil_phones` (`product_name`, `manufacture`, `product_count`, `price`) VALUES ('Galaxy S8', 'Samsung', '1', '41000');
+INSERT INTO `hw`.`mobil_phones` (`product_name`, `manufacture`, `product_count`, `price`) VALUES ('P20 Pro', 'Huawei', '5', '36000');
